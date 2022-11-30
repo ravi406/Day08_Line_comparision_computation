45a723a4da80b3f8d86c05b51807ff8d764ae5d8
ravi406 <ravikamble406@gmail.com>
2022-11-30 17:21:17 +0530

    Added : greeting message 'welcome to Line comparision computational program'

---
+++ src/com/day8/linecomp/LineComparision.java
@@ -0,0 +1,8 @@
+package com.day8.linecomp;
+
+public class LineComparision {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to LineComparision computational program");
+    }
+}
