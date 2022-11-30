5b1a463577e963a14c6a8bf4443a4eb4a01714b1
ravi406 <ravikamble406@gmail.com>
2022-11-30 19:00:37 +0530

    Added : logic for the comparision of two line with compareTo method is used in program

--- src/com/day8/linecomp/LineComparision.java
+++ src/com/day8/linecomp/LineComparision.java
@@ -17,26 +17,40 @@ public class LineComparision {
         y2 = scanner.nextInt();
 
         System.out.println("Enter co-ordinates of x3 and y3");
-        x1 = scanner.nextInt();
-        y1 = scanner.nextInt();
+        x3 = scanner.nextInt();
+        y3 = scanner.nextInt();
         System.out.println("Enter co-ordinates of x4 and y4");
-        x2 = scanner.nextInt();
-        y2 = scanner.nextInt();
+        x4 = scanner.nextInt();
+        y4 = scanner.nextInt();
     }
     private void calculateLength() {
         /* Computing the length of first line */
         double lengthOfFirstLine = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
         System.out.println("Length of first line is " + lengthOfFirstLine);
-        /* Computing the length of first line */
+        /* Computing the length of second line */
         double lengthOfSecondLine = Math.sqrt(Math.pow((x4 - x3), 2) + Math.pow((y4 - y3), 2));
-        System.out.println("Length of first line is " + lengthOfSecondLine);
+        System.out.println("Length of second line is " + lengthOfSecondLine);
     }
     /*checking the Equality of the lines*/
-    private void isEqual() {
+    private void isLineEqual() {
         if(lengthOfFirstLine == lengthOfSecondLine){
             System.out.println("Lines are Equal");
         }else {
             System.out.println("Lines are not Equal");
+        }
+    }
+
+    /*checking which line is greater using compare method*/
+    private void checkLineComparision() {
+        String str1 = Double.toString(lengthOfFirstLine);
+        String str2 = Double.toString(lengthOfSecondLine);
+        int valueOfLine = str1.compareTo(str2);
+        if(valueOfLine == lengthOfSecondLine){
+            System.out.println("Both Lines Are Equal ");
+        }else if(valueOfLine > lengthOfSecondLine){
+            System.out.println("First Line is Greater than Second Line");
+        }else {
+            System.out.println("Second line is Greater than First Line");
         }
     }
 
@@ -46,6 +60,7 @@ public class LineComparision {
         LineComparision lineComparision = new LineComparision();
         lineComparision.lineLengthValues();
         lineComparision.calculateLength();
-        lineComparision.isEqual();
+        lineComparision.isLineEqual();
+        lineComparision.checkLineComparision();
     }
 }
